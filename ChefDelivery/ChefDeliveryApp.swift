a917f15911dc85a3315468afc31ef230c97a44dc
Wellington Ruan da Silva <wellington_alcides@hotmail.com>
2024-06-30 21:05:57 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Wellington Ruan da Silva on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
