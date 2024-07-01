07ed5e4f824cc9c55b9f7f755ff005764c03e17e
Wellington Ruan da Silva <wellington_alcides@hotmail.com>
2024-06-30 21:44:43 -0300

    NavigationBar

--- ChefDelivery/NavigationBar.swift
+++ ChefDelivery/NavigationBar.swift
@@ -10,17 +10,40 @@ import SwiftUI
 struct NavigationBar: View {
     var body: some View {
         HStack {
-            Button("R. Vergueiro, 3185") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
+            Spacer()
             
-            Button("Notificações"){
+            Button("R. Vergueiro, 3185") {
                 
+            }
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.black)
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "bell.badge")
+                    .font(.title3)
+                    .foregroundColor(.red)
             }
         }
     }
 }
 
-#Preview {
-    NavigationBar()
+//Need to use struct preview to show things like . previewLayout
+struct NavigationBar_Preview: PreviewProvider {
+    static var previews: some View{
+        NavigationBar()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
 }
+
+//u cant see the static preview to show only the thing u want
+//#Preview {
+//    NavigationBar()
+//        .previewLayout(.sizeThatFits)
+//        .padding()
+//}
+
+
